592967fef12982857802af1f5547b7275f4030ef
Tony Williams <tonywilliams1986@gmail.com>
2014-02-14 18:55:32 +0000

    Used token replacement in nuspec and corrected version.fs namespace

--- src/AttemptIt/Version.fs
+++ src/AttemptIt/Version.fs
@@ -1,7 +1,8 @@
-﻿namespace AssemblyInfo
+﻿namespace AttemptIt
 
 open System.Reflection
 
 // use semantic versioning http://semver.org/
 [<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyCompany("Tony Williams")>]
 ()

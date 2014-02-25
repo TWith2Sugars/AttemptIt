7d8111f4fd1d18a318a25a479015b09e788ae3c3
Tony Williams <tonywilliams1986@gmail.com>
2014-02-25 15:54:33 +0000

    Tried out the formatting with F# power tools

--- src/AttemptIt.Tests/AttemptTests.fs
+++ src/AttemptIt.Tests/AttemptTests.fs
@@ -1,8 +1,8 @@
 ﻿module AttemptIt.Tests
 
-open System
 open AttemptIt
 open FsUnit.Xunit
+open System
 open Xunit
 
 let successValue x =
@@ -23,39 +23,56 @@ let isSuccess x =
 let isFailure x = isSuccess x |> not
 
 [<Fact>]
-let ``An empty attempt should be successful`` () =
-    attempt {return ()} |> Attempt.Run |> isSuccess |> should be True
+let ``An empty attempt should be successful``() =
+    attempt { return () }
+    |> Attempt.Run
+    |> isSuccess
+    |> should be True
 
 [<Fact>]
-let ``An attempt with an exception and no catch should still throw`` () =
-    (fun() -> attempt { raise(Exception("Should not be caught")); return ()} |>  Attempt.Run |> ignore) |> should throw typeof<Exception>
+let ``An attempt with an exception and no catch should still throw``() =
+    (fun () ->
+    attempt {
+        raise (Exception("Should not be caught"))
+        return ()
+    }
+    |> Attempt.Run
+    |> ignore)
+    |> should throw typeof<Exception>
 
 [<Fact>]
-let ``An attempt with an exception and a catch shoudld return Fail`` () =
-    let exceptionHandler (e:exn) = Fail e.Message
-    attempt { raise(Exception("Should be caught")); return ()} |> Attempt.Catch exceptionHandler |> Attempt.Run |> isFailure |> should be True
+let ``An attempt with an exception and a catch shoudld return Fail``() =
+    let exceptionHandler (e : exn) = Fail e.Message
+    attempt {
+        raise (Exception("Should be caught"))
+        return ()
+    }
+    |> Attempt.Catch exceptionHandler
+    |> Attempt.Run
+    |> isFailure
+    |> should be True
 
 [<Fact>]
-let ``A series of success attempts should result in an overall success`` () =
-    let successfullFunc x = attempt { return x+1 }
-    let overallAttempt =
-        attempt {
-            let! a = successfullFunc 1
-            let! b = successfullFunc a
-            let! c = successfullFunc b
-            return! successfullFunc c
-        }
-    overallAttempt |> Attempt.Run |> successValue |> should equal 5
+let ``A series of success attempts should result in an overall success``() =
+    let successfullFunc x = attempt { return x + 1 }
+    let overallAttempt = attempt { let! a = successfullFunc 1
+                                   let! b = successfullFunc a
+                                   let! c = successfullFunc b
+                                   return! successfullFunc c }
+    overallAttempt
+    |> Attempt.Run
+    |> successValue
+    |> should equal 5
 
 [<Fact>]
-let ``A series of success attempts that fail should result in an overall fail`` () =
-    let successfullFunc x = attempt { return x+1 }
+let ``A series of success attempts that fail should result in an overall fail``() =
+    let successfullFunc x = attempt { return x + 1 }
     let failFunc x = Fail x
-    let overallAttempt =
-        attempt {
-            let! a = successfullFunc 1
-            let! b = successfullFunc a
-            let! c = failFunc b
-            return! successfullFunc c
-        }
-    overallAttempt |> Attempt.Run |> failValue |> should equal 3
+    let overallAttempt = attempt { let! a = successfullFunc 1
+                                   let! b = successfullFunc a
+                                   let! c = failFunc b
+                                   return! successfullFunc c }
+    overallAttempt
+    |> Attempt.Run
+    |> failValue
+    |> should equal 3
--- src/AttemptIt/AttemptIt.fs
+++ src/AttemptIt/AttemptIt.fs
@@ -7,34 +7,40 @@ type Result<'TSuccess, 'TFail> =
     | Success of 'TSuccess
     | Fail of 'TFail
 
-type Attempt<'TSuccess, 'TFail> = (unit -> Result<'TSuccess,'TFail>)
+type Attempt<'TSuccess, 'TFail> = unit -> Result<'TSuccess, 'TFail>
 
 let internal succeed x = (fun () -> Success x)
 let internal failed x = (fun () -> Fail x)
-let internal runAttempt (a:Attempt<_,_>) = a()
-let internal delay f = (fun () -> runAttempt(f()))
+let internal runAttempt (a : Attempt<_, _>) = a()
+let internal delay f = (fun () -> runAttempt (f()))
 
-let internal either successTrack failTrack (input:Attempt<_,_>):Attempt<_,_> =
+let internal either successTrack failTrack (input : Attempt<_, _>) : Attempt<_, _> =
     match runAttempt input with
-        | Success s -> successTrack s
-        | Fail f -> failTrack f
+    | Success s -> successTrack s
+    | Fail f -> failTrack f
 
 let internal bind successTrack = either successTrack failed
 let internal fail failTrack result = either succeed failTrack result
 
 type Attempt =
-    static member Catch (handler:exn -> Result<_,_>) (x:Attempt<_,_>):Attempt<_,_> = fun _ -> try runAttempt x with e -> handler e
+
+    static member Catch (handler : exn -> Result<_, _>) (x : Attempt<_, _>) : Attempt<_, _> =
+        fun _ ->
+            try
+                runAttempt x
+            with e -> handler e
+
     static member Run x = runAttempt x
 
 type AttemptBuilder() =
-    member this.Bind(m:Attempt<_,_>, success) = bind success m
-    member this.Bind(m:Result<_,_>, success) = bind success (fun () -> m)
-    member this.Return(x) : Attempt<_,_>  = succeed x
-    member this.ReturnFrom(x:Attempt<_,_>) = x
-    member this.Combine(v, f):Attempt<_,_> = bind f v
+    member this.Bind(m : Attempt<_, _>, success) = bind success m
+    member this.Bind(m : Result<_, _>, success) = bind success (fun () -> m)
+    member this.Return(x) : Attempt<_, _> = succeed x
+    member this.ReturnFrom(x : Attempt<_, _>) = x
+    member this.Combine(v, f) : Attempt<_, _> = bind f v
     member this.Yield(x) = Success x
     member this.YieldFrom(x) = x
-    member this.Delay(f):Attempt<_,_> = delay f
-    member this.Zero() : Attempt<_,_> = succeed()
+    member this.Delay(f) : Attempt<_, _> = delay f
+    member this.Zero() : Attempt<_, _> = succeed()
 
 let attempt = AttemptBuilder()
